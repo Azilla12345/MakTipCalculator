e8e49228893257a341f779fd5985e2b1ebc45b6a
BT_1N3_14 <sample@bths.edu>
2023-09-21 10:39:38 -0400

    Now gives the total cost, tip, and costs per person before and after the tip.

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -11,10 +11,13 @@ public class TipCalculator {
         System.out.print("What's the the tip percentage (0-100): ");
         double tip = scan.nextDouble();
 
-        boolean repeat = true;
-
         double answer = 0.0;
-        double total_cost = 0;
+        double total_cost = 0.00;
+        double total_tip;
+        double full_cost;
+        double person_before_tip;
+        double person_after_tip;
+        double cost_per_person;
 
         while (answer != -1) {
             System.out.print("Enter the cost of the item (enter -1 once finished): ");
@@ -25,6 +28,19 @@ public class TipCalculator {
 
         }
 
-        System.out.print(total_cost);
+        total_tip = (total_cost / tip);
+        full_cost = (total_cost + total_tip);
+        person_before_tip = (total_cost / people);
+        person_after_tip = (total_tip / people);
+        cost_per_person = (full_cost / people);
+
+
+
+        System.out.println(total_cost);
+        System.out.println(total_tip);
+        System.out.println(full_cost);
+        System.out.println(person_before_tip);
+        System.out.println(person_after_tip);
+        System.out.println(cost_per_person);
     }
 }
