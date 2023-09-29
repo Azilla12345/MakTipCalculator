9fc4555fc64c73f814ab05f7753dcd85eddd6b11
bths computer <sample@bths.edu>
2023-09-29 10:50:53 -0400

    Now with comments!

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,25 +1,30 @@
 import java.util.Scanner;
 
-
 public class TipCalculator {
-
-
+        //formating
     public static String format(double price) {
         return String.format("%.2f", price);
     }
 
 
+    //the intro
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Hello, please enter your name: ");
         String name = scan.nextLine();
         System.out.println("Welcome to Tip Calculator, " + name);
+
+        System.out.print("In $, €, £, ¥, ₣, or ₹?: ");
+        String unit = scan.nextLine();
+
         System.out.print("Amount of people in group: ");
         int people = scan.nextInt();
         System.out.print("What's the the tip percentage (0-100): ");
         double tip = scan.nextDouble();
 
 
+
+        //variables are here
         double answer = 0.0;
         double total_cost = 0.00;
         double total_tip;
@@ -30,18 +35,21 @@ public class TipCalculator {
         double tax;
         double taxes;
 
-
+        //the code to ask the question
         while (answer != -1) {
             System.out.print("Enter the cost of the item (enter -1 once finished): ");
             answer = scan.nextDouble();
             if (answer != -1) {
                 total_cost += answer;
+
             }
+
+
 
 
         }
 
-
+        //math done here
         total_tip = (total_cost / tip);
         full_cost = (total_cost + total_tip);
         person_before_tip = (total_cost / people);
@@ -51,19 +59,16 @@ public class TipCalculator {
         taxes = (full_cost + tax);
 
 
-
-
-
-
-        System.out.println("Original cost: $" + format(total_cost));
+        //and finally, here we are
+        System.out.println("Original cost: " + unit + format(total_cost));
         System.out.println("Tip: " + tip + "%");
-        System.out.println("Tip cost: $" + format((total_tip)));
-        System.out.println("Total cost: $" + format(full_cost));
-        System.out.println("Cost per person before tip: $" + format(person_before_tip));
-        System.out.println("Cost per person for tip: $" + format(person_after_tip));
-        System.out.println("Cost per person after tip: $" + format(cost_per_person));
-        System.out.println("Taxes: " + format(tax));
-        System.out.println("Final cost: " + format(taxes));
+        System.out.println("Tip cost: " + unit + format((total_tip)));
+        System.out.println("Total cost: " + unit + format(full_cost));
+        System.out.println("Cost per person before tip: " + unit + format(person_before_tip));
+        System.out.println("Cost per person for tip: " + unit + format(person_after_tip));
+        System.out.println("Cost per person after tip: " + unit + format(cost_per_person));
+        System.out.println("Taxes: " + unit + format(tax));
+        System.out.println("Final cost: " + unit + format(taxes));
     }
 }
 
